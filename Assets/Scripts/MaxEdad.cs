0ce9c0cddeae5c56f947fc43d1f272c6a2518e5c
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-04-20 13:20:02 -0300

    cambios finales tp

---
+++ Assets/Scripts/MaxEdad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MaxEdad : MonoBehaviour
+{
+    public int edad;
+    public Text Text1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edad >= 18)
+        {
+            Text1.text=("Eres mayor de edad.");
+            Debug.Log("Eres mayor de edad.");
+        }
+        else
+        {
+            Debug.Log("No eres mayor de edad.");
+            Text1.text = ("Eres mayor de edad.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string ContraseñaCorrecta;
+    public Text IngresoUsuario;
+    string ContraseñaUsuario;
+    public GameObject cartelitomensaje;
+    public Text textomensaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ContraseñaCorrecta = "12345";
+        cartelitomensaje.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validaContraseña()
+    {
+        ContraseñaUsuario = IngresoUsuario.text;
+        if (ContraseñaUsuario == ContraseñaCorrecta)
+        {
+            cartelitomensaje.SetActive(true);
+            textomensaje.text = "Bienvenido";
+            Debug.Log("Bienvenido.");
+        }
+        else
+        {
+            cartelitomensaje.SetActive(true);
+            textomensaje.text = "Error, contraseña incorrecta.";
+            Debug.Log("Error, contraseña incorrecta.");
+        }
+    }
+}
